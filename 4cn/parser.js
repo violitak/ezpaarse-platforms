8cce7047b77d5d15631fe83a8d29b116090bdd11
Violita Kovchegov <66086030+violitak@users.noreply.github.com>
2026-08-17 13:57:15 -0400

    Update 4cn parser.js
    4cn: fix BOOK detection for /shelf2/ and directories containing digits, expose title_id

--- 4cn/parser.js
+++ 4cn/parser.js
@@ -20,11 +20,15 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   // console.error(parsedUrl);
   let match;
 
-  if ((match = /^\/shelf\/[a-z-]+\/([a-z0-9-_]+)\.html$/i.exec(path)) !== null) {
+  if ((match = /^\/shelf\d*\/([a-z0-9-_]+)\/([a-z0-9-_]+)\.html$/i.exec(path)) !== null) {
     //https://4canoesportal.org/shelf/Haida/TheHaidaofHaidaGwaii_2022-11-07_15-24-05.html
+    //https://4canoesportal.org/shelf2/MikmaqSpecialEdition/MikmaqSpecialEdition_2025-11-19_15-39-21.html
     result.rtype = 'BOOK';
     result.mime = 'HTML';
-    result.unitid = match[1];
+    // the shelf directory identifies the title and is stable, while the file name
+    // carries a publication timestamp that changes each time a title is re-published
+    result.title_id = match[1];
+    result.unitid = match[2];
   } else if (/^\/([a-z-]+)\/?$/i.test(path)) {
     // https://4canoes.com/focused-education-resources
     result.rtype = 'QUERY';
